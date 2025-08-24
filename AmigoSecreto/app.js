3eb7aa91cb4fb581b0506fc018f3c9d693f54f9d
RicardoACB <ricardo.chirinosg@gmail.com>
2025-08-23 22:48:39 -0400

    Resuelve errores al sortear sin contenido

--- AmigoSecreto/app.js
+++ AmigoSecreto/app.js
@@ -1,29 +1,26 @@
-let amigos=[]
-let lista_amigos=document.querySelector("#listaAmigos")
-let input_amigos=document.querySelector("#amigo")
-let submit=document.querySelector(".button-add")
-let sortear_amigo=document.querySelector(".button-draw")
-//let resultado=document.getElementsByTagName("resultado")
-let resultado=document.querySelector("#resultado")
-
-
+let amigos=[];
+let lista_amigos=document.querySelector("#listaAmigos");
+let input_amigos=document.querySelector("#amigo");
+let submit=document.querySelector(".button-add");
+let sortear_amigo=document.querySelector(".button-draw");
+let resultado=document.querySelector("#resultado");
 
 function agregar_amigo(){
     if (!input_amigos.value){
         alert("Tienes que poner un nombre para continuar");
     }
-    amigos.push(input_amigos.value);
-    input_amigos.value="";
-    mostrar_amigos()
-    resultado.innerHTML="";
-}
 
-function clear(variable){
-    lista_amigos.innerHTML=" ";
+    else{
+        amigos.push(input_amigos.value);
+        input_amigos.value="";
+        mostrar_amigos();
+        resultado.innerHTML="";
+    }
+   
 }
 
 function mostrar_amigos(){
-     let item2=document.createElement("LI")
+    let item2=document.createElement("LI");
     amigos.forEach(numero=>{
         item2.innerHTML=numero;
         lista_amigos.appendChild(item2);
@@ -31,18 +28,23 @@ function mostrar_amigos(){
 }
 
 function elegir_amigo(){
-    let item=document.createElement("LI")
-    amigo=Math.floor((Math.random()*amigos.length));
-    item.innerHTML=amigos[amigo];
-    resultado.appendChild(item);
-    amigos.length=0;
-}
+        if(amigos.length===0){
+            alert("Tiene que haber contenido dentro de la tombola");
+            resultado.innerHTML=" ";
+        }
+
+        else{
+            let item=document.createElement("LI")
+            amigo=Math.floor((Math.random()*amigos.length));
+            item.innerHTML=amigos[amigo];
+            resultado.appendChild(item);
+            amigos.length=0;
+        }
+    }
 
 function sortearAmigo(){
     lista_amigos.innerHTML="";
     elegir_amigo();
 }
-
-//INCLUIR FUNCIONES
 
 submit.setAttribute("onclick","agregar_amigo()");
